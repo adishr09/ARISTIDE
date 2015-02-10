5f06cb39991d0b272d4116adeffbc89d70896ab5
Aditya Dev Sharma <adi.shr09@gmail.com>
2015-02-10 22:18:41 +0530

    first page

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Aristdie - CMS </title>
+	<link rel="stylesheet" href="css/body.css" type="text/css">
+</head>
+<body>
+	<div class="top_bar">
+	<p> Login Status</p>
+	</div>
+	<div class="logo_bar">
+	<p> LOGO </p>
+	</div>
+	<div class="nav_bar">
+	<p> Nav Bar </p>
+	</div>
+	<div class="content">
+	<p> 
+		Hello Bitches 
+	</p>
+	</div>
+</body>
+</html>
